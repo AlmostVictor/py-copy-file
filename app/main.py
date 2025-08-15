61fa32a8aa9752c4c31824e1ff1a5391e586f58a
AlmostVictor <zhovtanetskyj.victor@gmail.com>
2025-08-15 20:35:14 +0300

    second fix

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 def copy_file(command : str) -> None:
+    if "/" in command or "\\" in command:
+        return
+
     parts = command.split()
+    if len(parts) == 3:
+        cmd, source_file_name, destination_file_name = parts
+    else:
+        return
+
+    if cmd != "cp" or source_file_name == destination_file_name:
+        return
+
     try:
-        if parts[0] != "cp" or parts[1] == parts[2]:
-            return
         with open(parts[1], "r") as source, open(parts[2], "w") as destination:
             destination.write(source.read())
-
-    except (IndexError, FileNotFoundError):
+    except FileNotFoundError:
         return
